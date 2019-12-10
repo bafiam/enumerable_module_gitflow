c6fdcf1de4c375bbbe0ca3a5b197df2f50a1b771
bafiam <stevebafiam@gmail.com>
2019-12-10 15:07:12 +0300

    correct issue identified by the TSE

--- enumerable.rb
+++ enumerable.rb
@@ -1,117 +1,137 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/ModuleLength
 module Enumerable
-  def my_each(arr)
+  def my_each
+    return self unless block_given?
+
     x = 0
-    while x < arr.length
-      yield(arr[x])
+    while x < length
+      yield(self[x])
       x += 1
     end
   end
 
-  def my_each_with_index(arr)
+  def my_each_with_index
+    return self unless block_given?
+
     i = 0
-    while i < arr.length
-      yield(arr[i], i)
+    while i < length
+      yield(self[i], i)
       i += 1
     end
   end
 
-  def my_select(arr)
+  def my_select
+    return self unless block_given?
+
     y = 0
     new_arr = []
-    while y < arr.length
-      new_arr.push(arr[y]) if yield(arr[y])
+    while y < length
+      new_arr.push(self[y]) if yield(self[y])
       y += 1
     end
     new_arr
   end
 
-  def my_all?(arr)
+  def my_all?
+    return self unless block_given?
+
     w = 0
-    while w < arr.length
-      return false unless yield(arr[w])
+    while w < length
+      return false unless yield(self[w])
 
       w += 1
     end
     true
   end
 
-  def any?(arr)
+  def my_any?
+    return self unless block_given?
+
     i = 0
-    while i < arr.length
-      return true if yield(arr[i])
+    while i < length
+      return true if yield(self[i])
 
       i += 1
     end
     false
   end
 
-  def my_none?(arr)
+  def my_none?
+    return self unless block_given?
+
     i = 0
-    while i < arr.length
-      return false if yield(arr[i])
+    while i < length
+      return false if yield(self[i])
 
       i += 1
     end
     true
   end
 
-  def my_count(arr)
+  def my_count
+    return length unless block_given?
+
     i = 0
     results = 0
-    while i < arr.length
-      results += 1 if yield(arr[i])
+    while i < length
+      results += 1 if yield(self[i])
       i += 1
     end
     results
   end
 
-  def my_map(arr)
+  def my_map
+    return self unless block_given?
+
     i = 0
     new_arr = []
-    while i < arr.length
-      new_arr.push(arr[i]) if yield(arr[i])
+    while i < length
+      new_arr.push(self[i]) if yield(self[i])
       i += 1
     end
     new_arr
   end
 
-  def my_inject(arr, results)
-    i = 0
-    while i < arr.length
+  def my_inject(results = 0)
+    return self unless block_given?
 
-      results = yield(results, arr[i])
+    i = 0
+    while i < length
+
+      results = yield(results, self[i])
       i += 1
     end
     results
   end
 
-  def multiply_els(arr, results)
-    my_inject(arr, results) { |result, num| result * num }
+  def multiply_els(results = 1)
+    my_inject(results) { |result, num| result * num }
   end
 
-  def my_map_modify(arr)
+  def my_map_modify
     i = 0
     new_arr = []
-    while i < arr.length
-      new_arr.push(arr[i]) if proc.call(arr[i])
+    while i < length
+      new_arr.push(self[i]) if proc.call(self[i])
       i += 1
     end
     new_arr
   end
 
-  def my_map_expanded(arr)
+  def my_map_expanded
     i = 0
     new_arr = []
-    while i < arr.length
+    while i < length
       if block_given?
-        new_arr.push(arr[i]) if yield(arr[i])
-      elsif proc.call(arr[i])
-        new_arr.push(arr[i])
+        new_arr.push(self[i]) if yield(self[i])
+      elsif proc.call(self[i])
+        new_arr.push(self[i])
       end
       i += 1
     end
     new_arr
   end
 end
+# rubocop:enable Metrics/ModuleLength
--- test.rb
+++ test.rb
@@ -1,94 +1,112 @@
 # frozen_string_literal: true
 
+require './enumerable.rb'
 # Test
 
 # ########my_each method
 
 arr = Array(1..10)
-Enumerable.my_each(arr) do |x|
-  puts x**4
-end
+arr.my_each { |x| puts x**4 }
+[1, 2, 3, 4, 5].my_each { |num| puts num }
+[1, 2, 3, 4, 5].my_each
 
 # ########my_each_with_index
 
 arr = %w[apple orange banana]
-Enumerable.my_each_with_index(arr) do |key, idx|
-  puts key.to_s if idx == 1
-end
+arr.my_each_with_index { |key, idx| puts key.to_s if idx == 1 }
+[1, 2, 3, 4, 5].my_each_with_index { |num| puts num }
 
-# ########my_select
+# # ########my_select
 
 fruits = %w[apple orange banana bacovo guava basa]
-puts(Enumerable.my_select(fruits) do |fruit|
+puts(fruits.my_select do |fruit|
   fruit.start_with? 'b'
 end)
+[1, 2, 3, 4, 5].my_select { |num| puts num }
+[1, 2, 3, 4, 5].my_select
 
-# ########my_myall?
+# # ########my_myall?
 
 arr = %w[vacua vagal vague vagus vails vairs]
 arr1 = Array(2..9)
 arr2 = Array(-1..7)
-puts(print('expected true :', Enumerable.my_all?(arr1) { |x| x > 1 })) # true
-puts(print('expected false :', Enumerable.my_all?(arr2) { |x| x > 1 })) # false, we have negative value which are < 1
-puts(print('expected true :', Enumerable.my_all?(arr) { |x| x.start_with? 'v' })) # true, all start with a v
+puts(print('expected true :', arr1.my_all? { |x| x > 1 })) # true
+puts(print('expected false :', arr2.my_all? { |x| x > 1 })) # false, we have negative value which are < 1
+puts(print('expected true :', arr.my_all? { |x| x.start_with? 'v' })) # true, all start with a v
+puts(print([1, 2, 3, 4, 5].my_all? { |num| num < 10 }))
+puts(print([1, 2, 3, 4, 5].my_all?))
+puts(print([1, 2, 3, 4, 5].my_all? { |i| i.is_a?(Integer) }))
+puts(print('regex :', %w[saheed oladele suretrust].my_all? { /d/ }))
 
-# ########my_any?
+# # ########my_any?
 
-puts(print(Enumerable.any?([2, 3, 1, 6, -4]) { |x| x <= 0 })) # return true, their is atleast "6">
+puts(print([2, 3, 1, 6, -4].my_any? { |x| x <= 0 })) # return true, their is atleast "6">
 arr = %w[vacua vagal vague vagus vails vairs]
-puts(print(Enumerable.any?(arr) { |x| x.start_with? 'y' })) # return false
+puts(print(arr.my_any? { |x| x.start_with? 'y' })) # return false
+puts(print([1, 2, 3, 4, 5].my_any? { |num| num < 10 }))
+puts(print([1, 2, 3, 4, 5].my_any?))
+puts(print([1, 2, 3, 4, 5].my_any? { |i| i.is_a?(Integer) })) # => true
+puts(print('regex :', %w[saheed oladele suretrust].my_any? { /d/ }))
 
-# ########my_none?
+# # ########my_none?
 
 arr = %w[ant bear cat]
-puts(print(Enumerable.my_none?(arr) { |word| word.length == 5 })) #=> true
-puts(print(Enumerable.my_none?(arr) { |word| word.length >= 4 })) #=> false
+puts(print(arr.my_none? { |word| word.length == 5 })) #=> true
+puts(print(arr.my_none? { |word| word.length >= 4 })) #=> false
+puts(print([1, 2, 3, 4, 5].my_none? { |num| num < 10 }))
+puts(print([1, 2, 3, 4, 5].my_none?))
+puts(print([1, 2, 3, 4, 5].my_none? { |i| i.is_a?(Integer) })) # => false
+puts(print('regex :', %w[saheed oladele suretrust].my_none? { /d/ }))
 
-# ########my_count?
+# # ########my_count?
 
 ary = [1, 2, 4, 2, 2, 2]
-puts(print(Enumerable.my_count(ary, &:even?))) #=> 5
-puts(print(Enumerable.my_count(ary) { |x| x == 2 })) #=> 4
+puts(print(ary.my_count(&:even?))) #=> 5
+puts(print(ary.my_count { |x| x == 2 })) #=> 4
+puts(print([1, 2, 3, 4, 5].my_count { |num| num == 4 })) #=> 1
+puts(print([1, 2, 3, 4, 5].my_count)) #=> 5
 
-# ########my_map
+# # ########my_map
 
 arr = Array(0...16)
 a = %w[albatross dog horse donkey]
-puts(print(Enumerable.my_map(arr) { |i| i >= 5 })) #=> [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-puts(print(Enumerable.my_map(a) { |word| word.start_with? 'd' })) #=> ["dog", "donkey"]
+puts(print(arr.my_map { |i| i >= 5 })) #=> [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+puts(print(a.my_map { |word| word.start_with? 'd' })) #=> ["dog", "donkey"]
+puts(print([1, 2, 3, 4, 5].my_map { |num| num < 3 }))
 
-# ########my_inject
+# # ########my_inject
 
 arr = Array(1..4)
 arr3 = Array(5..10)
 arr2 = Array(4..9) # [4,5,6,7,8,9]
-puts(print(Enumerable.my_inject(arr, 0) { |result, element| result + element })) #=> 10
-puts(print(Enumerable.my_inject(arr2, 0) { |result, element| result + element })) #=> 39
-puts(print(Enumerable.my_inject(arr3, 0) { |result, element| result + element })) #=> 45
+puts(print(arr.my_inject(0) { |result, element| result + element })) #=> 10
+puts(print(arr2.my_inject(0) { |result, element| result + element })) #=> 39
+puts(print(arr3.my_inject(0) { |result, element| result + element })) #=> 45
+puts(print([1, 2, 3, 4, 5].my_inject(0, &:+)))
 
-# ########multiply_els
+# # ########multiply_els
 
 arr = [2, 4, 5]
-puts(print(Enumerable.multiply_els(arr, 1))) #=> 40
+puts(print(arr.multiply_els)) #=> 40
 
-# ########my-map-modify-proc
+# # ########my-map-modify-proc
 
 arr = Array(0...16)
 a = %w[albatross dog horse donkey]
 map_proc1 = proc { |i| i >= 5 }
 map_proc2 = proc { |word| word.start_with? 'd' }
 
-puts(print(Enumerable.my_map_modify(arr, &map_proc1)))
-puts(print(Enumerable.my_map_modify(a, &map_proc2)))
+puts(print(arr.my_map_modify(&map_proc1)))
+puts(print(a.my_map_modify(&map_proc2)))
 
-# ########my_map_expanded
+# # ########my_map_expanded
 
-block && proc
+# block && proc
 
 arr = Array(0...16)
 a = %w[albatross dog horse donkey]
 map_proc2 = proc { |word| word.start_with? 'd' }
 #  #####block
-puts(print(Enumerable.my_map_expanded(arr) { |i| i >= 5 })) #=> [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+puts(print(arr.my_map_expanded { |i| i >= 5 })) #=> [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 # ######proc
-puts(print(Enumerable.my_map_expanded(a, &map_proc2))) #=> ["dog", "donkey"]
+puts(print(a.my_map_expanded(&map_proc2))) #=> ["dog", "donkey"]
